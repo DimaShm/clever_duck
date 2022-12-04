24d2d43fbe5866cb0100abb9d4e937e7eca7e61a
DimaShm <shmagindmitry@gmail.com>
2022-12-04 23:11:58 +0200

    Updates

--- static/js/main.dacdc399.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclever_duck=this.webpackJsonpclever_duck||[]).push([[0],{11:function(e,c,n){"use strict";n.r(c);var t=n(3),r=n.n(t),s=n(4),i=(n(9),n(1));var d=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("p",{children:"Hello"})})};r.a.render(Object(i.jsx)(s.a,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))},9:function(e,c,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.dacdc399.chunk.js.map
